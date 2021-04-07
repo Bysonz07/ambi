7cf1b26bfc6abe848299cab69beb6117a96a22ac
mughieams <mughieams24@gmail.com>
2021-04-07 10:14:23 +0700

    Initial Commit

---
+++ ambi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ambi
+//
+//  Created by Mughie Arief Mughoni on 07/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
