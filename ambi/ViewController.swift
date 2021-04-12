d05294a5371b4a0d0fe9d2a77a5bfade433d8f9d
mughieams <mughieams24@gmail.com>
2021-04-12 09:44:02 +0700

    feat(random): create simple random image

--- ambi/ViewController.swift
+++ ambi/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var imageRandom: UIImageView!
+    
+    let imageNames = ["Book", "Brush", "Crayon", "Eraser", "Glue", "Highlighter", "Paper", "Pen", "Pencil", "Ruler", "Sharpener", "Tape"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showImage(_ sender: Any) {
+        let number:Int = Int (arc4random_uniform(12));
+        
+        imageRandom.image = UIImage (named: imageNames[number]);
+    }
+    
 }
 
