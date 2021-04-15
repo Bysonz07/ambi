6808bffce8b5f73dee702b0d15fd641a1110643e
DifaNaufalPratama <dnpn07@gmail.com>
2021-04-15 10:31:51 +0700

    feat(develop):Final_V_sorryKetinggalanWkwwkw

--- ambi/ReviewController.swift
+++ ambi/ReviewController.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  ReviewController.swift
 //  ambi
@@ -5,35 +7,58 @@
 //  Created by Jehnsen Hirena Kane on 10/04/21.
 //
 
+
 import UIKit
 
 class ReviewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var outletReviewCollection: UICollectionView!
     @IBOutlet weak var outletSkipToCongrats: UIButton!
-    @IBOutlet weak var outletLabelTester2: UIButton!
+
+    @IBOutlet weak var homeButton: UIButton!
     @IBOutlet weak var labeltester2: UILabel!
+    
     var brain = AmbiBrain()
     let reuseIdentifier = "reviewCell" // also enter this string as the cell identifier in the storyboard
-    var items = ["crayon", "notes", "pencil", "penggares", "penghapus"]
-    var listOfImageString = ["crayon","notes","pencil", "penggares", "penghapus", "simbolSuara"]
+    var items = ["crayon", "Book", "Pencil", "Ruler", "Eraser"]
+    var listOfImageString = Card.nameChosen
     var listOfImages : [UIImage] = []
+    var textArray : [String] = []
+    var imageChosen : [String] = []
     
    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        subtleAnimate()
+        homeButton.isHidden = true
+        outletReviewCollection.isHidden = false
         brain.animateOpacity(outletReviewCollection.layer)
         inputImage()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 200, height: 200)
+        outletReviewCollection.collectionViewLayout = layout
+        outletReviewCollection.backgroundColor = .clear
+        outletReviewCollection.delegate = self
+        outletReviewCollection.dataSource = self
+        outletReviewCollection.allowsMultipleSelection = true
     }
     
-    
-    @IBAction func bubarJalan(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+
     @IBAction func skipToCongrats(_ sender: UIButton) {
+        homeButton.isHidden = false
+        outletSkipToCongrats.isHidden = true
+        outletReviewCollection.isHidden = true
         congratsMuncul()
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) { [self] in
+            
+//            brain.playSound(musicName: "earworm", musicExtension: "mp3")
+//
+            performSegue(withIdentifier: "homePage", sender: nil)
+            
+        }
+//        performSegue(withIdentifier: "homePage", sender: nil)
     }
     
     
@@ -42,6 +67,8 @@ class ReviewController : UIViewController, UICollectionViewDelegate, UICollectio
         return self.listOfImageString.count
     }
     
+    
+    //MARK: MASUKIN CELL
     // make a cell for each cell index path
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
@@ -62,6 +89,22 @@ class ReviewController : UIViewController, UICollectionViewDelegate, UICollectio
         print("You selected cell #\(indexPath.item)!")
         labeltester2.text = listOfImageString[indexPath.item]
         print(listOfImageString[indexPath.item])
+        brain.playSound(musicName: "audio" + String(labeltester2.text!).capitalized)
+        
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.clear
+        textArray.append(listOfImageString[indexPath.item])
+        print(textArray)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+        let cell = collectionView.cellForItem(at: indexPath)
+        brain.playSound(musicName: "audio" + String(labeltester2.text!).capitalized)
+        cell?.backgroundColor = UIColor.clear
+        textArray = textArray.filter { $0 != listOfImageString[indexPath.item] }
+        print(textArray)
     }
     
     // change background color when user touches cell
@@ -79,8 +122,8 @@ class ReviewController : UIViewController, UICollectionViewDelegate, UICollectio
     func inputImage() {
 //        listOfImages = [(UIImage (named: listOfImageString[0]))]
         
-            for i in 0...listOfImageString.count - 1 {
-                listOfImages.append(UIImage (named: listOfImageString[i])!)
+        for i in Card.nameChosen {
+            listOfImages.append(UIImage (named: i + "Outline")!)
             }
     }
     
@@ -91,7 +134,6 @@ class ReviewController : UIViewController, UICollectionViewDelegate, UICollectio
         
         
         labeltester2.text = ""
-        outletLabelTester2.alpha = 0
         outletSkipToCongrats.alpha = 0
         outletReviewCollection.alpha = 0
         outletImageCongrats.image = UIImage (named: "congrats.png")
@@ -99,4 +141,22 @@ class ReviewController : UIViewController, UICollectionViewDelegate, UICollectio
         brain.animateScale(outletImageCongrats.layer)
     }
     
+    func subtleAnimate(){
+        let emitter = Emitter.getEmitter(with: UIImage(imageLiteralResourceName: "triangle"), range: 45)
+        emitter.emitterPosition = CGPoint(x: view.frame.width/4, y: view.frame.height)
+        emitter.emitterSize = CGSize(width: view.frame.width, height: 2)
+        view.layer.insertSublayer(emitter, at: 0)
+        let emitter2 = Emitter.getEmitter(with: UIImage(imageLiteralResourceName: "circle"), range: 15)
+        emitter2.emitterPosition = CGPoint(x: view.frame.width/4, y: view.frame.height)
+        emitter2.emitterSize = CGSize(width: view.frame.width, height: 2)
+        view.layer.insertSublayer(emitter2, at: 0)
+    }
+    
 }
+
+extension ReviewController {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
